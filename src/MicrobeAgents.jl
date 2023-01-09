289f19204323d63f7e9529426bfbae2ead9b1c4f
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-01-09 22:18:48 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/MicrobeAgents.jl
@@ -0,0 +1,5 @@
+module MicrobeAgents
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MicrobeAgents
+using Test
+
+@testset "MicrobeAgents.jl" begin
+    # Write your tests here.
+end
