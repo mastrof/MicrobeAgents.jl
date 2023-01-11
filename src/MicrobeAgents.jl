fff3e5be976be3d68f62089154cad963645a2ff7
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-01-11 18:26:53 +0100

    export `AbstractMicrobe` from module

--- src/MicrobeAgents.jl
+++ src/MicrobeAgents.jl
@@ -7,10 +7,31 @@ using Random
 using Rotations
 using StaticArrays
 
+
+export AbstractMicrobe
+"""
+    AbstractMicrobe{D} <: AbstractAgent where {D<:Integer}
+All microbe types in MicrobeAgents.jl simulations must be instances
+of user-defined types that are subtypes of `AbstractMicrobe`.
+    YourMicrobeType{D} <: AbstractMicrobe{D}
+The parameter `D` defines the dimensionality of the space in which the
+microbe type lives (1, 2 and 3 are supported).
+
+All microbe types *must* have at least the following fields:
+- `id::Int` id of the microbe (used internally by Agents.jl)
+- `pos::NTuple{D,Float64}` position of the microbe
+- `vel::NTuple{D,Float64}` velocity of the microbe
+- `motility::AbstractMotility` motile pattern of the microbe
+- `turn_rate::Real` average reorientation rate of the microbe
+- `rotational_diffusivity::Real` coefficient of brownian rotational diffusion
+- `radius::Real` equivalent spherical radius of the microbe
+- `state::Real` generic variable for a scalar internal state
+"""
+abstract type AbstractMicrobe{D} <: AbstractAgent where D end
+
 include("utils.jl")
 include("distributions.jl")
 include("motility.jl")
 include("microbes.jl")
 include("rotations.jl")
-
 end
