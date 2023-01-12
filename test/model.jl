f984005eaa03a666f2e4d686eba8e40e9c6491bd
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-01-12 10:14:52 +0100

    add model stepping tests

--- test/model.jl
+++ test/model.jl
@@ -41,6 +41,9 @@ using LinearAlgebra: norm
             @test model[1].turn_rate == 0.55
             @test model[1].motility isa RunReverse
             @test norm(model[1].vel) ≈ 24.0
+            vel = rand_vel(D, RunTumble())
+            add_agent!(model; vel)
+            @test model[2].vel == vel
 
             # add agent to given position
             model = ABM(Microbe{D}, extent, timestep)
@@ -61,6 +64,57 @@ using LinearAlgebra: norm
     end
 
     @testset "Stepping" begin
-        
+        for D in 1:3
+            dt = 1
+            extent = ntuple(_ -> 300, D)
+            model = ABM(Microbe{D}, extent, dt)
+            pos = extent ./ 2
+            vel1 = rand_vel(D, RunTumble())
+            add_agent!(pos, model; vel=vel1, turn_rate=0)
+            vel2 = rand_vel(D, RunReverse())
+            add_agent!(pos, model; vel=vel2, turn_rate=Inf, motility=RunReverse())
+            run!(model) # performs 1 microbe_step! and 1 model.update! step
+            # x₁ = x₀ + vΔt
+            @test all(model[1].pos .≈ pos .+ vel1.*dt)
+            @test all(model[2].pos .≈ pos .+ vel2.*dt)
+            # v is the same for the agent with zero turn rate
+            @test all(model[1].vel .≈ vel1)
+            # v is changed for the other agent
+            @test ~all(model[2].vel .≈ vel2)
+            # and since it turned its motile state changed from Forward to Backward
+            @test model[2].motility.state == Backward
+            # model time counter was increased to 1
+            @test model.t == 1
+            # perform other 5 steps
+            run!(model, 5)
+            @test model.t == 6
+
+            # customize microbe affect! function
+            # decreases microbe state value by D at each step
+            MicrobeAgents.affect!(microbe::Microbe{D}, model) = (microbe.state-=D)
+            model = ABM(Microbe{D}, extent, dt)
+            add_agent!(model)
+            run!(model)
+            @test model[1].state == -D
+            # restore default affect! function
+            MicrobeAgents.affect!(microbe::Microbe{D}, model) = MicrobeAgents._affect!(microbe, model)
+            run!(model)
+            # now the state has not changed from previous step
+            @test model[1].state == -D
+
+            # customize model.update! function
+            model = ABM(Microbe{D}, extent, dt)
+            tick_more!(model::ABM) = (model.t += 3)
+            model → tick_more! # now model.t increases by 4 (+1 +3) at each step
+            run!(model, 6)
+            @test model.t == 6*4
+            decrease!(model::ABM) = (model.t -= 1)
+            model = ABM(Microbe{D}, extent, dt)
+            # chain arbitrary number of functions
+            model → tick_more! → decrease! → decrease! → decrease!
+            # now we should be back at only +1 per step
+            run!(model, 20)
+            @test model.t == 20
+        end
     end
 end
