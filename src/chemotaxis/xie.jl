860c0c939036818d3242cf5fd259265c1ac9641c
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-02-05 22:31:47 +0100

    add docstring to Xie and remove superfluous AbstractXie type

--- src/chemotaxis/xie.jl
+++ src/chemotaxis/xie.jl
@@ -1,8 +1,35 @@
 export AbstractXie, Xie, XieNoisy
 
-abstract type AbstractXie{D} <: AbstractMicrobe{D} end
+"""
+    Xie{D} <: AbstractMicrobe{D}
+Model of chemotactic bacterium adapted from 'Xie et al. (2019) Biophys J'.
+The model is developed based on experimental measurements of the
+chemotactic response function in the marine bacterium V. alginolyticus.
+The peculiarity of the model is the presence of distinct parameters for the
+forward and backward swimming states.
 
-mutable struct Xie{D} <: AbstractXie{D}
+Sensing noise (not present in the original model) is customarily introduced
+through the molecular counting noise formula by Berg and Purcell, and can be
+tuned through a `chemotactic_precision` factor inspired by
+'Brumley et al. (2019) PNAS' (defaults to 0, i.e. no noise).
+
+Default parameters:
+- `motility = RunReverseFlick(speed_forward = [46.5])`
+- `turn_rate_forward = 2.3` Hz
+- `turn_rate_backward = 1.9` Hz
+- `state = 0.0` s
+- `state_m = 0.0` s
+- `state_z = 0.0` s
+- `rotational_diffusivity = 0.26` rad²/s
+- `adaptation_time_m = 1.29` s
+- `adaptation_time_z = 0.28` s
+- `gain_forward = 2.7` 1/s
+- `gain_backward = 1.6` 1/s
+- `binding_affinity = 0.39` μM
+- `chemotactic_precision = 0.0`
+- `radius = 0.5` μm
+"""
+mutable struct Xie{D}
     id::Int
     pos::NTuple{D,Float64}
     motility::AbstractMotility
@@ -54,7 +81,7 @@ end # struct
 
 
 # needs a different show because it does not have a "turn_rate" field
-function Base.show(io::IO, ::MIME"text/plain", m::AbstractXie{D}) where D
+function Base.show(io::IO, ::MIME"text/plain", m::Xie{D}) where D
     println(io, "$(typeof(m)) with $(typeof(m.motility)) motility")
     println(io, "position (μm): $(r2dig.(m.pos)); velocity (μm/s): $(r2dig.(m.vel.*m.speed))")
     println(io, "average unbiased turn rate (Hz): forward $(r2dig(m.turn_rate_forward)), backward $(r2dig(m.turn_rate_backward))")
@@ -62,14 +89,16 @@ function Base.show(io::IO, ::MIME"text/plain", m::AbstractXie{D}) where D
     print(io, "other properties: " * join(s, ", "))
 end
 
-function turnrate(microbe::AbstractXie, model)
+# Xie requires its own turnrate functions
+# since it has different parameters for fw and bw states
+function turnrate(microbe::Xie, model)
     if microbe.motility isa AbstractMotilityTwoStep
         return turnrate_twostep(microbe, model)
     else
         return turnrate_onestep(microbe, model)
     end
 end
-function turnrate_twostep(microbe::AbstractXie, model)
+function turnrate_twostep(microbe::Xie, model)
     S = microbe.state
     if microbe.motility.state == Forward
         ν₀ = microbe.turn_rate_forward
@@ -80,20 +109,21 @@ function turnrate_twostep(microbe::AbstractXie, model)
     end
     return ν₀*(1 + β*S)
 end
-function turnrate_onestep(microbe::AbstractXie, model)
+function turnrate_onestep(microbe::Xie, model)
     S = microbe.state
     ν₀ = microbe.turn_rate_forward
     β = microbe.gain_forward
     return ν₀*(1 + β*S)
 end
 
-function affect!(microbe::AbstractXie, model; ε=1e-16)
+function affect!(microbe::Xie, model; ε=1e-16)
     Δt = model.timestep
     Dc = model.compound_diffusivity
     c = model.concentration_field(microbe.pos, model)
     K = microbe.binding_affinity
     a = microbe.radius
     Π = microbe.chemotactic_precision
+    # noisy concentration measurement with Berg-Purcell formula
     σ = Π * 0.04075 * sqrt(3*c / (5*π*Dc*a*Δt))
     M = rand(Normal(c,σ))
     ϕ = log(1.0 + max(M/K, -1+ε))
