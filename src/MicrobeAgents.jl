7a8df90a979651c5b25217c08975f8521b357352
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-03-20 15:02:52 +0100

    comment

--- src/MicrobeAgents.jl
+++ src/MicrobeAgents.jl
@@ -45,7 +45,7 @@ include("chemotaxis/brown-berg.jl")
 include("chemotaxis/brumley.jl")
 include("chemotaxis/xie.jl")
 
-# bodies (for obstacles, encounters...)
+# obstacles, encounters, pathfinding...
 using GeometryBasics: HyperSphere, Point
 include("bodies/spheres.jl")
 using Agents.Pathfinding
