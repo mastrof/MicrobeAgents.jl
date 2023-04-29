8dfd9d542f5eda2f30c6652689911c47550a02af
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-04-29 11:21:30 +0200

    example rw1d

---
+++ examples/RandomWalks/randomwalk1D.jl
@@ -0,0 +1,30 @@
+using MicrobeAgents
+using Plots
+
+## model parameters
+L = 1000
+extent = (L,)
+dt = 0.1
+n = 10
+nsteps = 600
+##
+
+## abm setup
+model = UnremovableABM(Microbe{1}, extent, dt)
+foreach(_ -> add_agent!((0,), model), 1:n)
+##
+
+## simulation
+adata = [:pos]
+adf, _ = run!(model, nsteps; adata)
+##
+
+## postprocessing
+traj = MicrobeAgents.unfold(vectorize_adf_measurement(adf,:pos), L)
+x = first.(traj)
+t = axes(x,1) .* dt
+##
+
+## visualization
+plot(t, x, leg=false, xlab="time", ylab="displacement")
+##
