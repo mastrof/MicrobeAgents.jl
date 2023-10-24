db52b6e00c12c86d0fcad50aa73d3cfd7c205aea
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-10-25 00:06:43 +0200

    fix stepping tests

--- test/model-stepping.jl
+++ test/model-stepping.jl
@@ -16,7 +16,7 @@ using LinearAlgebra: norm
         vel2 = random_velocity(model)
         speed2 = random_speed(rng, RunReverse())
         add_agent!(pos, model; vel=vel2, speed=speed2, turn_rate=Inf, motility=RunReverse())
-        run!(model) # performs 1 microbe_step! and 1 model.update! step
+        run!(model, 1) # performs 1 microbe_step!
         # x₁ = x₀ + vΔt
         @test all(model[1].pos .≈ pos .+ vel1 .* speed1 .* dt)
         @test all(model[2].pos .≈ pos .+ vel2 .* speed2 .* dt)
@@ -26,37 +26,24 @@ using LinearAlgebra: norm
         @test ~all(model[2].vel .≈ vel2)
         # and since it turned its motile state changed from Forward to Backward
         @test model[2].motility.state == Backward
-        # model time counter was increased to 1
-        @test model.t == 1
-        # perform other 5 steps
-        run!(model, 5)
-        @test model.t == 6
 
         # customize microbe affect! function
         # decreases microbe state value by D at each step
         MicrobeAgents.affect!(microbe::Microbe{D}, model) = (microbe.state -= D)
         model = ABM(Microbe{D}, space, dt)
         add_agent!(model)
-        run!(model)
+        run!(model, 1)
         @test model[1].state == -D
         # restore default affect! function
         MicrobeAgents.affect!(microbe::Microbe{D}, model) = nothing
-        run!(model)
+        run!(model, 1)
         # now the state has not changed from previous step
         @test model[1].state == -D
 
-        # customize model.update! function
-        model = ABM(Microbe{D}, space, dt)
-        tick_more!(model) = (model.t += 3)
-        model → tick_more! # now model.t increases by 4 (+1 +3) at each step
+        # customize model_step! function
+        model_step!(model) = model.t += 1
+        model = ABM(Microbe{D}, space, dt; model_step!)
         run!(model, 6)
-        @test model.t == 6 * 4
-        decrease!(model) = (model.t -= 1)
-        model = ABM(Microbe{D}, space, dt)
-        # chain arbitrary number of functions
-        model → tick_more! → decrease! → decrease! → decrease!
-        # now we should be back at only +1 per step
-        run!(model, 20)
-        @test model.t == 20
+        @test model.t == 6
     end
 end
