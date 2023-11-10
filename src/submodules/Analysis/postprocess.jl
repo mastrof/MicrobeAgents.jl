fb9cc1e3455244cb30d52b19c31bc694ae83a6c1
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-11-10 13:53:20 +0100

    replace `vectorize_adf_measurement` with `adf_to_vectors` and `adf_to_matrix`

--- src/submodules/Analysis/postprocess.jl
+++ src/submodules/Analysis/postprocess.jl
@@ -1,11 +1,13 @@
-export vectorize_adf_measurement
+export adf_to_matrix, adf_to_vectors
 
 """
-    vectorize_adf_measurement(adf, sym)
-Collect quantity `sym` from the agent dataframe `adf` and return it in matrix 
-form with dimensions (times, microbes).
+    adf_to_matrix(adf, sym)
+Collect quantity `sym` from the agent dataframe `adf` and return it
+in matrix form with dimensions (times, microbes).
+Requires all microbes to exist from start to end of the simulations 
+(no removals or insertions).
 """
-function vectorize_adf_measurement(adf, sym)
+function adf_to_matrix(adf, sym)
     nmicrobes = unique(adf[!,:id]) |> length
     nsteps = unique(adf[!,:step]) |> length
     datatype = typeof(adf[1,sym])
@@ -13,7 +15,18 @@ function vectorize_adf_measurement(adf, sym)
     for i in 1:nmicrobes
         for t in 1:nsteps
             s[t,i] = adf[i + (t-1)*nmicrobes, sym]
-        end # for
-    end # for
+        end
+    end
     return s
-end # function
+end
+
+"""
+    adf_to_vectors(adf, sym)
+Collect quantity `sym` from the agent dataframe `adf` and return a
+vector of such quantity for each microbe id.
+"""
+function adf_to_vectors(adf, sym)
+    gdf = groupby(adf, :id)
+    s = [g[!,:sym] for g in gdf]
+    return s
+end
