bfa6db054d0abcceb4cf01845b702592ba1a50af
Riccardo Foffi <riccardo.foffi@protonmail.com>
2024-05-18 01:08:42 +0200

    update `microbe_step!`

--- src/microbe_step.jl
+++ src/microbe_step.jl
@@ -5,21 +5,29 @@ export microbe_step!, microbe_pathfinder_step!
 Perform an integration step for `microbe`. In order:
 1. Update `microbe` position according to its current velocity
 2. Reorient `microbe` through rotational diffusion
-3. Update the state of `microbe` through a dedicated `affect!` function
-4. Evaluate instantaneous turn rate through dedicated `turnrate` function
-5. Reorient `microbe` if necessary
+3. Update internal state of `microbe` through a customizable `affect!` function
+4. Reorient `microbe` (if the motile state has a reorientation component)
+5. Update motile state of microbe (transitions occur as Poisson events)
 """
 function microbe_step!(microbe::AbstractMicrobe, model::AgentBasedModel)
     dt = abmtimestep(model)
     move_agent!(microbe, model, speed(microbe)*dt) # translation
     rotational_diffusion!(microbe, model) # rotational noise
-    turn!(microbe, model) # reorientation
     model.affect!(microbe, model) # update microbe's internal state
-    # probability to switch motile state
-    p = dt * bias(microbe) / duration(motilepattern(microbe))
+    turn!(microbe, model) # reorientation
+    p = switching_probability(microbe, model)
     if rand(abmrng(model)) < p
+        # transition to new motile state
         update_motilestate!(microbe, model)
     end
+end
+
+function switching_probability(microbe, model)
+    dt = abmtimestep(model)
+    M = motilestate(microbe)
+    τ = duration(M)
+    β = biased(M) ? bias(microbe) : 1.0
+    return β * dt / τ
 end
 
 """
