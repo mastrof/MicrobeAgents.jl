f90bbff4ecda82392e03a374104afe5b5ad479b4
Riccardo Foffi <riccardo.foffi@protonmail.com>
2024-05-18 01:12:21 +0200

    update pathfinder step

--- src/microbe_step.jl
+++ src/microbe_step.jl
@@ -22,31 +22,28 @@ function microbe_step!(microbe::AbstractMicrobe, model::AgentBasedModel)
     end
 end
 
+"""
+    microbe_pathfinder_step!(microbe, model)
+Identical to `microbe_step!` except that motion is constrained by the pathfinder
+(`model.pathfinder`) which defines inaccessible regions of space.
+"""
+function microbe_pathfinder_step!(microbe::AbstractMicrobe, model::AgentBasedModel)
+    dt = abmtimestep(model)
+    pathfinder_step!(microbe, model, dt) # translation
+    rotational_diffusion!(microbe, model) # rotational noise
+    model.affect!(microbe, model) # update microbe's internal state
+    turn!(microbe, model) # reorientation
+    p = switching_probability(microbe, model)
+    if rand(abmrng(model)) < p
+        # transition to new motile state
+        update_motilestate!(microbe, model)
+    end
+end
+
 function switching_probability(microbe, model)
     dt = abmtimestep(model)
     M = motilestate(microbe)
     τ = duration(M)
     β = biased(M) ? bias(microbe) : 1.0
     return β * dt / τ
-end
-
-"""
-    microbe_pathfinder_step!(microbe, model)
-Identical to `microbe_step!` except that motion is constrained by the pathfinder
-(`model.pathfinder`) which defines inaccessible regions of space.
-"""
-function microbe_pathfinder_step!(microbe::AbstractMicrobe, model)
-    dt = model.timestep # integration timestep
-    # update microbe position
-    pathfinder_step!(microbe, model, dt)
-    # reorient through rotational diffusion
-    rotational_diffusion!(microbe, model)
-    # update microbe state
-    model.affect!(microbe, model)
-    # evaluate instantaneous turn rate
-    ω = turnrate(microbe) * tumblebias(microbe)
-    if rand(abmrng(model)) < ω * dt # if true reorient microbe
-        turn!(microbe, model)
-    end
-    nothing
 end
